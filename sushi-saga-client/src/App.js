75cb564642d7de08be765341a9698eac56e4bf09
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-06-09 10:32:24 -0400

    30 minute mark

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,10 +7,33 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushis: [],
+    start: 0,
+    end: 4,
+  }
+
+  getSushis = () => {
+    fetch(API)
+      .then(res => res.json())
+      .then(json => this.setState({sushis: json}))
+  }
+
+  componentDidMount() {
+    this.getSushis()
+  }
+
+  eatSushi = (sushi) => {
+    console.log(sushi)
+  }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer />
+        <SushiContainer 
+          sushis={this.state.sushis.slice(this.state.start, this.state.end)} 
+          eatSushi={this.eatSushi}
+        />
         <Table />
       </div>
     );
